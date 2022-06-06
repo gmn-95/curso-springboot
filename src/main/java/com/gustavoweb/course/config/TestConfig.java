357fae7964b8599b8238a020f4c5a46a41d14fa5
Gustavo Macedo <gmnascimento.95@outlook.com.br>
2022-06-06 20:55:11 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/gustavoweb/course/config/TestConfig.java
+++ src/main/java/com/gustavoweb/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.gustavoweb.course.entities.Category;
 import com.gustavoweb.course.entities.Order;
+import com.gustavoweb.course.entities.OrderItem;
 import com.gustavoweb.course.entities.Product;
 import com.gustavoweb.course.entities.User;
 import com.gustavoweb.course.entities.enums.OrderStatus;
 import com.gustavoweb.course.repositories.CategoryRepository;
+import com.gustavoweb.course.repositories.OrderItemRepository;
 import com.gustavoweb.course.repositories.OrderRepository;
 import com.gustavoweb.course.repositories.ProductRepository;
 import com.gustavoweb.course.repositories.UserRepository;
@@ -22,11 +24,8 @@ import com.gustavoweb.course.repositories.UserRepository;
 @Profile("test") //configuração especifica ao perfil de teste
 public class TestConfig implements CommandLineRunner{
 	
-	/*
-	 * 
-	 * 
-	 * */
-	@Autowired //injeção de dependência
+	//injeção de dependência
+	@Autowired 
 	private UserRepository userRepository;
 	
 	@Autowired
@@ -37,6 +36,9 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -72,8 +74,12 @@ public class TestConfig implements CommandLineRunner{
 		
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
 	}
-	
-	
 }
