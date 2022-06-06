4421016c1b1d39bc8d07a6f9003599264bf7f5ab
Gustavo Macedo <gmnascimento.95@outlook.com.br>
2022-06-06 01:57:48 -0300

    Service layer, component registration

---
+++ src/main/java/com/gustavoweb/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.gustavoweb.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gustavoweb.course.entities.User;
+import com.gustavoweb.course.repositories.UserRepository;
+
+@Service //registrar a class como um componente do spring, para injeção de dependência
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
